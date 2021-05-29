41a32875a93e90e6239bf48d639ce30d0aefe3b0
Phuc Nguyen <phucnguyenhong93@gmail.com>
2021-05-29 19:45:41 +0700

    Config middleware

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,30 @@
 import { Route } from "core/interfaces";
 import express, { Router } from "express";
 import mongoose, { connect } from "mongoose";
+import hpp from "hpp";
+import morgan from "morgan";
+import cors from "cors";
+import helmet from "helmet";
+import { Logger } from "./core/utils";
 
 class App {
   public app: express.Application;
   public port: string | number;
+  public production: boolean;
 
   constructor(routes: Route[]) {
     this.app = express();
     this.port = process.env.PORT || 5000;
+    this.production = process.env.NODE_ENV == "production" ? true : false;
 
     this.initializeRoutes(routes);
     this.connectToDatabase();
+    this.initializeMiddleware();
   }
 
   public listen() {
     this.app.listen(this.port, () => {
-      console.log(`Server is listening on port ${this.port}`);
+      Logger.info(`Server is listening on port ${this.port}`);
     });
   }
 
@@ -26,23 +34,35 @@ class App {
     });
   }
 
-  private connectToDatabase() {
-    try {
-      const cnn = process.env.MONGODB_URI;
-      if (!cnn) {
-        console.log("Connection string invalid");
-        return;
-      }
-      mongoose.connect(cnn, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
-      });
-      console.log("Database connected...");
-    } catch (error) {
-      console.log("Connect to database error");
+  private initializeMiddleware() {
+    if (this.production) {
+      this.app.use(hpp());
+      this.app.use(helmet());
+      this.app.use(morgan("combined"));
+      this.app.use(
+        cors({ origin: "your.domain.com", credentials: true }),
+      );
+    } else {
+      this.app.use(morgan("dev"));
+      this.app.use(cors({ origin: true, credentials: true }));
     }
+  }
+
+  private connectToDatabase() {
+    const cnn = process.env.MONGODB_URI;
+    if (!cnn) {
+      Logger.error("Connection string is invalid");
+      return;
+    }
+    mongoose.connect(cnn, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    }).catch((reason) => {
+      Logger.error(reason);
+    });
+    Logger.info("Database connected...");
   }
 }
 
